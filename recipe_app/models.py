a4aeef639734317ff22de326276bff8f44978434
HelniseS <helnise-santos12@hotmail.com>
2025-11-08 22:56:41 +0000

    Add Tag model with name field and ordering

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.text import slugify
+
 # Create your models here.
+User = get_user_model()
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
