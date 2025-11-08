e3327a891ca4ce748f058c17e79b47a8bcf64f74
HelniseS <helnise-santos12@hotmail.com>
2025-11-08 22:36:03 +0000

    Fix URL routing by adding accounts path for authentication

--- globalrecipe/urls.py
+++ globalrecipe/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path( 'accounts/', include('django.contrib.auth.urls'))
     path('', include('recipe_app.urls')),
 ]
