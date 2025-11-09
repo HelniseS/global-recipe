ffae0691386572beabc163c1767ccf598f91b06c
HelniseS <helnise-santos12@hotmail.com>
2025-11-09 14:56:13 +0000

    Add HomeView class to display recipes with pagination and tags

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -8,3 +8,18 @@ from .models import Recipe, Tag
 from .forms import RecipeForm, NutritionForm, IngredientFormSet, StepFormSet
 
 
+class HomeView(ListView):
+    template_name = "recipe_app/home.html"
+    context_object_name = "recipes"
+    paginate_by = 8
+
+    def get_queryset(self):
+        return Recipe.objects.select_related("author").prefetch_related("tags").order_by("-created_at")[:8]
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["tags"] = Tag.objects.all()
+        return ctx
+
+
+
