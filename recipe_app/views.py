f3fe3c0a1afe26c627a36cc22d9fe427267f054e
HelniseS <helnise-santos12@hotmail.com>
2025-11-09 13:26:57 +0000

    Add necessary imports to views.py for recipe handling

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# recipe_app/views.py
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, View
+from .models import Recipe, Tag
+from .forms import RecipeForm, NutritionForm, IngredientFormSet, StepFormSet
 
-# Create your views here.
+
