814498bbfe778b5817367f7faa42ae8fd7a2f6bd
HelniseS <helnise-santos12@hotmail.com>
2025-11-09 03:53:46 +0000

    add form clsses and formsets or recipe creation

---
+++ recipe_app/forms.py
@@ -0,0 +1,32 @@
+# recipe_app/forms.py
+from django import forms
+from django.forms import inlineformset_factory
+from .models import Recipe, Nutrition, Ingredient, Step
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ["title", "category", "description", "cooking_time_min", "servings", "image", "tags"]
+
+class NutritionForm(forms.ModelForm):
+    class Meta:
+        model = Nutrition
+        fields = ["calories", "protein_g", "carbs_g", "fat_g"]
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = ["text"]
+
+class StepForm(forms.ModelForm):
+    class Meta:
+        model = Step
+        fields = ["order", "text"]
+
+IngredientFormSet = inlineformset_factory(
+    Recipe, Ingredient, form=IngredientForm, extra=1, can_delete=True
+)
+
+StepFormSet = inlineformset_factory(
+    Recipe, Step, form=StepForm, extra=1, can_delete=True
+)
