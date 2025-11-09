1e352fa807109bf18a64718c0f2be1cc15b6b2dd
HelniseS <helnise-santos12@hotmail.com>
2025-11-09 00:32:45 +0000

    Register Ingredient model in admin with inline editing

--- recipe_app/admin.py
+++ recipe_app/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import Recipe, Ingredient, Step, Nutrition, Tag
 
 # Register your models here.
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+    extra = 0
