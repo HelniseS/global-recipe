084fc4f2c3727628f1250d50e1e6a1dce30df669
HelniseS <helnise-santos12@hotmail.com>
2025-11-17 18:19:09 +0000

    Fix indentation in urlpatterns and ensure proper formatting

--- recipe_app/urls.py
+++ recipe_app/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 app_name = "recipe_app"
 
+
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('recipes/', views.RecipeListView.as_view(), name='recipe_list'),
-    path('recipes/<slug:slug>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
     path('recipes/add/', views.RecipeCreateView.as_view(), name='recipe_add'),
+    path('recipes/<slug:slug>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
 ]
