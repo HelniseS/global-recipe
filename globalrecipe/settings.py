b97c3fc3266b868a760f637480108a9e01dbcc2b
HelniseS <helnise-santos12@hotmail.com>
2025-11-22 22:37:05 +0000

    Update LOGOUT_REDIRECT_URL to point to recipe_app:home

--- globalrecipe/settings.py
+++ globalrecipe/settings.py
@@ -131,4 +131,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'recipe_app:home'
