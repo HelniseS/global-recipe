cbd49cd5945fc75d3589f3ce4d540e206a0e7e82
HelniseS <helnise-santos12@hotmail.com>
2025-11-23 04:06:22 +0000

    Add URL patterns for recipe management features

--- recipe_app/urls.py
+++ recipe_app/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('recipes/', views.RecipeListView.as_view(), name='recipe_list'),
     path('recipes/add/', views.RecipeCreateView.as_view(), name='recipe_add'),
     path('recipes/<int:pk>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
+    path('recipes/mine/', views.MyRecipesView.as_view(), name='my_recipes'),
+    path('recipes/<int:pk>/edit/', views.RecipeUpdateView.as_view(), name='recipe_edit'),
+    path('recipes/<int:pk>/delete/', views.RecipeDeleteView.as_view(), name='recipe_delete'),
+
 ]
