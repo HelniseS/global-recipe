0b97727188019e7d994e04aa177b90ec31d80a81
HelniseS <helnise-santos12@hotmail.com>
2025-11-27 17:42:40 +0000

    Use env SECRET_KEY

--- globalrecipe/settings.py
+++ globalrecipe/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-235eq6c%&2g!$1i9avem^g4h!hb+$cgjwxs1)#u)h86mf*i4ah'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
